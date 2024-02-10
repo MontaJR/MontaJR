a5468209e8032366cffaf1e974894a16f900c26f
Slowyy <62079462+MontaJR@users.noreply.github.com>
2024-02-10 18:10:06 +0100

    Delete ComandosMonta directory

--- ComandosMonta/client.lua
+++
@@ -1,6 +0,0 @@
-RegisterCommand("discord", function()
-    TriggerEvent ('chatMessage', 'admin', {0,255,0}, "discord.gg/tudiscord")
-
-    RegisterCommand("web", function()
-        TriggerEvent ('chatMessage', 'admin', {0,255,0}, "https://tupaginaweb.es")
-end)
